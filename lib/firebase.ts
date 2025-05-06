11f2ccacd1e8bbb8da845bc4e5710a15ed4cfbc1
petronem <petronem@glx.sh>
2025-05-06 21:38:21 +0000

    .env

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,19 +1,19 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import * as dotenv from "dotenv";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Load environment variables from .env
+dotenv.config();
+
+// Firebase configuration using environment variables
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API,
-  authDomain: "resumeit-auth.firebaseapp.com",
-  projectId: "resumeit-auth",
-  storageBucket: "resumeit-auth.appspot.com",
-  messagingSenderId: "479650561287",
-  appId: "1:479650561287:web:6a007bc0fd3f235f4cd9a2",
-  measurementId: "G-8830XX5JVP"
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
